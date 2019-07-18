dd0692184b71cf395e40711cab9be7370517f8ee
Victor Ramirez <VRamirez75@gmail.com>
2019-07-18 00:47:48 -0700

    Added a running serverto Node Express lab 4 project.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
+// import Express library
 const express = require("express");
+// variable to access the methods
+// exposes a top-level function
 const app = express();
 
+// creating a “route”
 app.get("/", function(req, res){
 
     res.send("it works!");
 
 });
 
+// allow the server to listen for any request
 // server listener
-app.listen("8081", "0.0.0.0", function() {
+app.listen("8081", "127.0.0.1", function() {
 
     console.log("Express Server is Running...")
 
